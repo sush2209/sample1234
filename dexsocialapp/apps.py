b853c1a081e444b2b234e5f33efa3c310fb7883e
Sushant Appasaheb Shelake <70313065+sush2209@users.noreply.github.com>
2024-10-01 01:43:15 +0530

    Update apps.py

--- dexsocialapp/apps.py
+++ dexsocialapp/apps.py
@@ -5,7 +5,7 @@ class DexsocialappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dexsocialapp'
 
-    def ready(self):
-        if os.environ.get('RUN_MAIN') == 'true':  # Prevent running the scheduler multiple times
-            from .scheduler import start_scheduler  # Import the correct function
-            start_scheduler()  # Call the correct function to start the scheduler
+    # def ready(self):
+    #     if os.environ.get('RUN_MAIN') == 'true':  # Prevent running the scheduler multiple times
+    #         from .scheduler import start_scheduler  # Import the correct function
+    #         start_scheduler()  # Call the correct function to start the scheduler
